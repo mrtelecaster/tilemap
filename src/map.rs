635baaff09ecb60d632f26081f5bd826242d2206
Fernando A. Fraticelli <53105809+mrtelecaster@users.noreply.github.com>
2023-02-10 14:34:54 -0500

    refactor(map): add doc comments and some whitespace
    #37

--- src/map.rs
+++ src/map.rs
@@ -4,6 +4,9 @@ use std::{collections::HashMap, hash::Hash};
 use crate::hex::AxialCoords;
 
 
+// TILEMAP STRUCT ------------------------------------------------------------------------------- //
+
+/// A structure that can hold a map of tiles at arbitrary coordinates
 pub struct TileMap<C, T>
 {
 	map: HashMap<C, T>,
@@ -11,20 +14,31 @@ pub struct TileMap<C, T>
 
 impl<C, T> TileMap<C, T>
 {
-	pub fn new() -> Self {
+	/// Creates a new `TileMap` with no tiles
+	pub fn new() -> Self
+	{
 		Self{ map: HashMap::new() }
 	}
 
-	pub fn get_tile(&self, coord: &C) -> Option<&T> where C: Eq + Hash {
+	/// Gets the tile that's at the given coordinates. If there is no tile at those coordinates,
+	/// `None` is returned.
+	pub fn get_tile(&self, coord: &C) -> Option<&T> where C: Eq + Hash
+	{
 		self.map.get(coord)
 	}
 
-	pub fn set_tile(&mut self, coord: C, tile: T) where C: Eq + Hash {
-		self.map.insert(coord, tile);
+	/// Insert a new tile into the map at the given coordinates.
+	/// 
+	/// If there is already a tile there, it will be replaced by the new tile, with the old tile
+	/// data returned by the function.
+	pub fn insert_tile(&mut self, coord: C, tile: T) -> Option<T> where C: Eq + Hash
+	{
+		self.map.insert(coord, tile)
 	}
 }
 
 
+// MAP ALIASES ---------------------------------------------------------------------------------- //
 
 /// Tile map using hexagonal coordinates
 pub type HexMap<T> = TileMap<AxialCoords, T>;
